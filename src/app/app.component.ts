854d65b6b894b6e66caf89577e4287a9c90f8936
Paulo Murakami <paulo.murakami@afinzpartners.com.br>
2024-11-29 11:18:34 -0300

    feat - adicionando o sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fed-murakami-pay';
+  showSidebar = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      const excludedRoutes = ['/login', '/error'];
+      this.showSidebar = !excludedRoutes.includes(this.router.url);
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,5 @@ import { AuthGuard } from './guards/auth.guard';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Uncomment this line
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Uncomment this line
 ];
